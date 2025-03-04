9bfeb1d9c6f3bbe3f871376646ba9e693ddeff47
Nandhakumar Palanisamy <nandhakumar.palanisamy@vuori.com>
2025-03-04 07:53:08 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,27 +1,17 @@
-const cspHeader = `
-    default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline';
-    style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data:;
-    font-src 'self';
-    object-src 'none';
-    base-uri 'self';
-    form-action 'self';
-    frame-ancestors 'none';
-`
- 
+const { getCSPDirectives, getCSPHeader } = require('./config/cspConfig');
+
 module.exports = {
   async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: cspHeader.replace(/\n/g, ''),
-          },
-        ],
-      },
-    ]
-  },
+      return [
+        {
+          source: '/:path*',
+          headers: [
+            {
+              key: getCSPHeader(),
+              value: getCSPDirectives(),
+            },
+          ],
+        },
+      ];
+    },
 }
